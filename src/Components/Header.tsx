2077fcd1bffcba917b4588a1105dba286add5f10
danilo1opes <contatolopesdan@gmail.com>
2025-08-19 10:09:46 -0300

    🎨feat: Create Sidenav and SVG title

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,10 +3,13 @@ import DateRange from './DateRange';
 import { Meses } from './Meses';
 
 export const Header = () => {
+  const [title, setTitle] = React.useState('Resumo');
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="mb daterange">
         <DateRange />
+        <h1 className="box bg-3">{title}</h1>
       </div>
       <Meses />
     </header>
